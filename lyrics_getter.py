f702307b480d1e5a9509ba78e825afe7b0db8977
prononoob <lollel60606@gmail.com>
2022-09-22 11:37:40 +0200

    getting titles

--- lyrics_getter.py
+++ lyrics_getter.py
@@ -1,4 +1,4 @@
-import requests
+import requests, re
 from bs4 import BeautifulSoup
 
 
@@ -7,7 +7,15 @@ class Getter:
 		print('Hello!')
 		self.f = open('lyrics.txt', 'w')
 		self.properlink	= 'https://www.azlyrics.com/l/lilpeep.html'
-		r = requests.get(self.properlink)
+		self.r = requests.get(self.properlink)
+		self.soup = str(BeautifulSoup(self.r.content, 'html.parser')).split('\n')
+
+	def titles(self):
+		for i in self.soup:
+			if 'div class="listalbum-item"><a href="/lyrics/lilpeep' in i:
+				print(i[38:])
+				self.f.write(i[38:] + '\n')
+
 
 	def writetest(self):
 		self.f.write(self.properlink)
@@ -16,7 +24,8 @@ class Getter:
 
 def main():
 	g = Getter()
-	g.writetest()
+	g.titles()
+	#g.writetest()
 
 
 if __name__ == '__main__':
