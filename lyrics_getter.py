872b0314680bebc8d6506f643f5b50efb233c324
prononoob <lollel60606@gmail.com>
2022-09-23 22:16:19 +0200

    added randomTitle and randomLyric methods

--- lyrics_getter.py
+++ lyrics_getter.py
@@ -1,4 +1,4 @@
-import requests
+import requests, random
 from bs4 import BeautifulSoup
 
 
@@ -10,12 +10,13 @@ class Getter:
 		self.properlink	= 'https://www.azlyrics.com/l/lilpeep.html'
 		self.r = requests.get(self.properlink)
 		self.soup = str(BeautifulSoup(self.r.content, 'html.parser'))
-
-	def titles(self):
 		self.soup = self.soup[self.soup.find('songlist'):].split('\n')
 		self.soup = self.soup[1:]
 		self.soup = '\n'.join(self.soup)
 		self.soup = self.soup[:self.soup.find(']')].split('\n')
+		self.maxRange = len(self.soup)
+
+	def getTitles(self):
 		for i in self.soup:
 			i = i[i.find('"')+1:]
 			h = i[i.find('h:')+3:]
@@ -27,6 +28,29 @@ class Getter:
 			self.content = i + ' ' + h + '\n'
 			self.f.write(self.content)
 
+	def randomTitle(self):
+		self.x = random.randint(0, self.maxRange)
+		self.link = self.soup[self.x]
+		self.link = self.link[self.link.find('h:')+3:]
+		self.link = self.link[:self.link.find('html')+4]
+		if 'https' not in self.link:
+			self.link = self.azlyrics + self.link
+		print(self.link)
+
+	def randomLyrics(self):
+		self.r2 = requests.get(self.link)
+		self.lyrics = str(BeautifulSoup(self.r2.content, 'html.parser'))
+		self.lyrics = self.lyrics[self.lyrics.find('Sorry about that')+23:]
+		self.lyrics = self.lyrics[:self.lyrics.find('</div>')-1]
+		self.lyrics = self.lyrics.split('\n')
+		self.maxLyrRange = random.randint(0, len(self.lyrics))
+		self.lyrics = self.lyrics[self.maxLyrRange]
+		self.lyrics = self.lyrics.strip('<br/>')
+		print(self.lyrics)
+
+	def lirycs(self):
+		pass
+
 
 	def writetest(self):
 		self.f.write(self.properlink)
@@ -35,7 +59,8 @@ class Getter:
 
 def main():
 	g = Getter()
-	g.titles()
+	g.randomTitle()
+	g.randomLyrics()
 	#g.writetest()
 
 
